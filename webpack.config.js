9a397ff0249b37d932e08fa89b8aebcb83dc43e3
yuanmingxiang <1154322556@qq.com>
2020-08-18 13:19:19 +0800

    添加url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const devMode = process.env.NODE_ENV === 'development'; // 是否是开发模式
@@ -13,10 +13,26 @@ module.exports = {
         path: path.resolve(__dirname, 'build'),
         //输出的文件名称
         filename: 'bundle.js',
+        // 输出文件路径前缀
         publicPath: '/build/'
     },
     module: {
-        rules: [{
+        rules: [
+            // url-loader把图片转成base64，和bound.js打包到一起
+            // {
+            //     test: /\.(png|jpg|gif|bmp)$/,
+            //     use: [{
+            //         loader: 'url-loader',
+            //         options: {
+            //             name: '[name]_[hash:8].[ext]',
+            //             // 写法同 name: 'image/[name]_[hash:8].[ext]',
+            //             outputPath: 'image',
+            //             // 超过limit就使用file-loader
+            //             limit: 14000,
+            //         }
+            //     }]
+            // },
+            {
                 test: /\.(png|jpg|gif)$/,
                 use: [{
                     loader: 'file-loader',
@@ -29,16 +45,17 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: [{
-                        // 单独打成css包
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: '../'
-                        }
-                    },
+                use: [
+                    // 单独打成css包
+                    // {
+                    //     loader: MiniCssExtractPlugin.loader,
+                    //     options: {
+                    //         publicPath: '../'
+                    //     }
+                    // },
 
-                    // 样式会被打包到head的style标签中
-                    // 'style-loader',
+                    // 使用style-loader样式会被打包到head的style标签中
+                    'style-loader',
                     "css-loader",
                     'postcss-loader',
                     'less-loader',
@@ -59,10 +76,11 @@ module.exports = {
     },
     plugins: [
         // 打包后把css抽离成单独的文件
-        new MiniCssExtractPlugin({
-            filename: "css/[name]_[hash:8].css",
-            chunkFilename: "[id].css"
-        }),
+        // new MiniCssExtractPlugin({
+        //     filename: "css/[name]_[hash:8].css",
+        //     chunkFilename: "[id].css"
+        // }),
+
         // 自动删除output.path文件
         new CleanWebpackPlugin(),
         // 自动生成html打包模板，并且自动引入js和css
